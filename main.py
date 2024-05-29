82e9f495a527f3e3d9fa044e4347856794b6e129
fernandojusviak <fernandojusviak@gmail.com>
2024-05-28 21:24:29 -0300

    criar uma lista bebidas

---
+++ main.py
@@ -0,0 +1,17 @@
+menu = [
+    {"nome": "Coca-Cola", "pr": 4, "categoria": "bebidas", "sub_categoria": "refrigerante"},
+    {"nome": "Guaraná Antarctica", "pr": 3.5, "categoria": "bebidas", "sub_categoria": "refrigerante"},
+    {"nome": "Suco de Laranja Natural", "pr": 7, "categoria": "bebidas", "sub_categoria": "suco"},
+    {"nome": "Suco de Maracujá Natural", "pr": 8, "categoria": "bebidas", "sub_categoria": "suco"},
+    {"nome": "Chá Gelado de Limão", "pr": 5.5, "categoria": "bebidas", "sub_categoria": "chá gelado"},
+    {"nome": "Chá Gelado de Pêssego", "pr": 5.5, "categoria": "bebidas", "sub_categoria": "chá gelado"},
+    {"nome": "Cerveja Skol", "pr": 5, "categoria": "bebidas", "sub_categoria": "bebida alcoólica"},
+    {"nome": "Cerveja Heineken", "pr": 7, "categoria": "bebidas", "sub_categoria": "bebida alcoólica"},
+    {"nome": "Red Bull", "pr": 10, "categoria": "bebidas", "sub_categoria": "bebida energética"},
+    {"nome": "Monster Energy", "pr": 12, "categoria": "bebidas", "sub_categoria": "bebida energética"},
+    {"nome": "Café Expresso", "pr": 4, "categoria": "bebidas", "sub_categoria": "bebida quente"},
+    {"nome": "Cappuccino", "pr": 7, "categoria": "bebidas", "sub_categoria": "bebida quente"},
+    {"nome": "Água Mineral com Gás", "pr": 3, "categoria": "bebidas", "sub_categoria": "água"},
+    {"nome": "Água Mineral sem Gás", "pr": 2.5, "categoria": "bebidas", "sub_categoria": "água"},
+    {"nome": "Água de Coco", "pr": 6, "categoria": "bebidas", "sub_categoria": "água"}
+]
